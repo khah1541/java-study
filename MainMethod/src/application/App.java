951f74dfac6ae203975cb71e2e5fb3840d301246
khah <khah1541@naver.com>
2021-10-13 12:29:44 +0900

    10월13일 오전

--- MainMethod/src/application/App.java
+++ MainMethod/src/application/App.java
@@ -4,6 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		// 메인메소드에서 프로그램 시작
+		// 자동정렬 컨트롤 + 쉬프트 + F 
 		System.out.println("안녕하세요");
 		System.out.println("새 자바 프로그램을");
 		System.out.println("실행 합니다.");
---
+++ ch02_Variables/src/convertingTemp/App.java
@@ -0,0 +1,13 @@
+package convertingTemp;
+
+public class App {
+
+	public static void main(String[] args) {
+		// ¼·¾¾ Celsius => È­¾¾ Fahrenheit 
+		double c = 25;
+		double f = (c* 9/5) + 32;
+					
+				System.out.println("¼·¾¾  "+c+"´Â È­¾¾"+f+"ÀÌ´Ù.");
+	}
+
+}
